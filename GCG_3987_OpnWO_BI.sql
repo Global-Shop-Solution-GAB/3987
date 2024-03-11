510dcecca7f1a833c5dc3447da90e471050c3b61
Service Web <sw@gssmail.com>
2024-03-11 10:16:29 -0500

    Commit changes in C:\Websites\Git\emarlow\3987\GCG_3987_OpnWO_BI.sql

---
+++ GCG_3987_OpnWO_BI.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "GCG_3987_OpnWO_BI"(
+ "Job" CHAR(6),
+ "Suffix" CHAR(3),
+ "Seq" CHAR(6),
+ "Part" CHAR(20),
+ "Qty" NUMERIC(12,4),
+ "Due_Date" DATE,
+ "Employee" CHAR(30) NOT NULL ,
+ "BIRunID" BIGINT,
+ "OpCode" CHAR(10));
